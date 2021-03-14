ae21350305664faf0204dc20968399ed7b1cc2aa
farhan-nazir <nazirf3440@gmail.com>
2021-03-14 01:37:32 +0100

    new change

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
 import { TuiButton } from "lib/Button";
 import { TuiTypography } from "lib/Typography";
-export { TuiButton, TuiTypography };
+import App from "../tui/src/App";
+export { TuiButton, TuiTypography, App };
