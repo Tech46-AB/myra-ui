f8c5da35867571de4fcaa5fe64ee144954536a97
farhan-nazir <nazirf3440@gmail.com>
2021-03-15 22:37:37 +0100

    storybook typograpgy add

--- myra/.storybook/main.js
+++ myra/.storybook/main.js
@@ -1,5 +1,9 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.md",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
